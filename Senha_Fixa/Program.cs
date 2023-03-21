b57bc0db22873b250d23f92413a6bd264f93b87a
bezerrafernando <fernandodbezerra@gmail.com>
2023-03-21 12:21:57 -0300

    Repeticao_Enquanto

---
+++ Senha_Fixa/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+Console.WriteLine("Deus é Bom todo tempo!!!");
+
+int senha = int.Parse(Console.ReadLine());
+
+while(senha != 2002){
+    Console.WriteLine("Senha invalida");
+    senha = int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Acesso Permitido");
